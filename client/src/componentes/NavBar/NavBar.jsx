a15a95ade4d1fed7fb859ec88773136dc4078b92
kevin170703 <correakevinfabian01@gmail.com>
2022-12-07 23:09:56 -0300

    responsive navbar

--- client/src/componentes/NavBar/NavBar.jsx
+++ client/src/componentes/NavBar/NavBar.jsx
@@ -7,22 +7,41 @@ import { useDispatch, useSelector } from "react-redux";
 import Swal from "sweetalert2";
 import { searchName } from "../../redux/actions";
 import { useState } from "react";
+import { HiBars3, HiXMark } from "react-icons/hi2";
 
 export default function Navbar({ home, services }) {
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const navegation = useNavigate();
   const [nameSearch, setNameSearch] = useState("");
+  const [phoneVist, setPhoneVist] = useState(false);
 
   function dataSearch(e) {
     e.preventDefault();
     dispatch(searchName(nameSearch));
     setNameSearch("");
+    setPhoneVist(false);
+    navegation("/home");
   }
 
   return (
-    <div className={style.content}>
-      <div className={style.navegation}>
+    <div className={style.contentTotal}>
+      <div className={style.buttons}>
+        <HiBars3
+          size="30"
+          className={phoneVist === true ? style.butonMenuNone : style.butonMenu}
+          onClick={() => setPhoneVist(true)}
+        />
+
+        <HiXMark
+          size="30"
+          className={
+            phoneVist === false ? style.butonMenuNone : style.butonMenu
+          }
+          onClick={() => setPhoneVist(false)}
+        />
+      </div>
+      <div className={style.navegationSec}>
         <NavLink to="/" className={home === true ? style.border : style.inicio}>
           inicio
         </NavLink>
@@ -34,51 +53,70 @@ export default function Navbar({ home, services }) {
         </NavLink>
       </div>
 
-      <form className={style.searchBar} onSubmit={(e) => dataSearch(e)}>
-        <input
-          type="text"
-          placeholder="Buscar servicios..."
-          value={nameSearch}
-          onChange={(e) => setNameSearch(e.target.value)}
-        />
-        <button type="submit">{<CiSearch size="30" />}</button>
-      </form>
+      {/* ------------------------------------------- */}
 
-      <div className={style.sesionAndPosts}>
-        {user[0]?.msj || !user.length ? (
-          <NavLink to="/login" className={style.sesion}>
-            Iniciar sesion
-          </NavLink>
-        ) : (
-          <NavLink to="/profile">
-            <IoPersonOutline size="30" className={style.logoProfile} />
-          </NavLink>
-        )}
-        {!user.length ? (
+      <div className={phoneVist === true ? style.contentNone : style.content}>
+        <div className={style.navegation}>
           <NavLink
-            className={style.newPost}
-            onClick={() =>
-              Swal.fire({
-                icon: "info",
-                title: "debes iniciar sesion para publicar servicios",
-                confirmButtonText: "Iniciar sesion",
-                confirmButtonColor: "#7b2cbf",
-                showCancelButton: true,
-                cancelButtonText: "Hacerlo mas tarde",
-              }).then((result) => {
-                if (result.isConfirmed) {
-                  navegation("/login");
-                }
-              })
-            }
+            to="/"
+            className={home === true ? style.border : style.inicio}
           >
-            Publicar servicio
+            inicio
           </NavLink>
-        ) : (
-          <NavLink to={"/createPublic"} className={style.newPost}>
-            Publicar servicio
+          <NavLink
+            to="/home"
+            className={services === true ? style.border : style.service}
+          >
+            Buscar servicios
           </NavLink>
-        )}
+        </div>
+
+        <form className={style.searchBar} onSubmit={(e) => dataSearch(e)}>
+          <input
+            type="text"
+            placeholder="Buscar servicios..."
+            value={nameSearch}
+            onChange={(e) => setNameSearch(e.target.value)}
+          />
+          <button type="submit">{<CiSearch size="30" />}</button>
+        </form>
+
+        <div className={style.sesionAndPosts}>
+          {user[0]?.msj || !user.length ? (
+            <NavLink to="/login" className={style.sesion}>
+              Iniciar sesion
+            </NavLink>
+          ) : (
+            <NavLink to="/profile">
+              <IoPersonOutline size="30" className={style.logoProfile} />
+            </NavLink>
+          )}
+          {!user.length ? (
+            <NavLink
+              className={style.newPost}
+              onClick={() =>
+                Swal.fire({
+                  icon: "info",
+                  title: "debes iniciar sesion para publicar servicios",
+                  confirmButtonText: "Iniciar sesion",
+                  confirmButtonColor: "#7b2cbf",
+                  showCancelButton: true,
+                  cancelButtonText: "Hacerlo mas tarde",
+                }).then((result) => {
+                  if (result.isConfirmed) {
+                    navegation("/login");
+                  }
+                })
+              }
+            >
+              Publicar servicio
+            </NavLink>
+          ) : (
+            <NavLink to={"/createPublic"} className={style.newPost}>
+              Publicar servicio
+            </NavLink>
+          )}
+        </div>
       </div>
     </div>
   );
