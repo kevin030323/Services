d4e9d33a1c4f5704dd6b6043b6e3d397505813c9
kevin170703 <correakevinfabian01@gmail.com>
2022-12-20 13:48:21 -0300

    landing v2

--- client/src/componentes/Landing/Landing.jsx
+++ client/src/componentes/Landing/Landing.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
-import sytle from "./Landing.module.css";
+import style from "./Landing.module.css";
 import fondo from "../../assets/undraw_coffee_break_h3uu.svg";
+import { NavLink } from "react-router-dom";
 
 export default function Landing() {
   return (
-    <div>
+    <div className={style.contetAll}>
       <NavBar home={true} />
-      <div className={sytle.content}>
-        <div className={sytle.contentText}>
+
+      <div className={style.content}>
+        <div></div>
+        <div className={style.contentText}>
           <h1>Services</h1>
           <h5>
             Plataforma para encontrar y publicar servicios, de manera rápida y
             sencilla.
           </h5>
         </div>
-        <div className={sytle.contentImg}>
+        <div className={style.contentImg}>
           <img src={fondo} alt="" />
         </div>
+        <div className={style.wave}></div>
       </div>
     </div>
   );
