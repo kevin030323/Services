628141de371cf4bb9068a5ce66b01b8a1843568f
kevin170703 <correakevinfabian01@gmail.com>
2023-02-03 16:22:53 -0300

    problema de login solucuinado

--- api/src/controllers/accounts.js
+++ api/src/controllers/accounts.js
@@ -7,11 +7,11 @@ async function login(req, res) {
   const userLogin = user !== null ? user.password === password : false;
 
   if (!userLogin) {
-    return res.status(404).json({
+    return res.json({
       msj: "La contraseña o el numero de telefono no coinciden",
     });
   }
-  res.status(200).json(user);
+  res.status(200).json([user]);
 }
 
 async function register(req, res) {
